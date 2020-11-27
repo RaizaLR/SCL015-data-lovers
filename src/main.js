7e014ad46c8d96f8a00ce978846614030d3a5180
RaizaLR <raizalongart@gmail.com>
2020-11-27 19:26:02 -0300

    agregamos lateral de filtros a pagina de personajes

--- src/main.js
+++ src/main.js
@@ -11,3 +11,12 @@ charactersBtn.addEventListener("click", function(){
     const charactersSection = document.getElementById("charactersSection");
     charactersSection.style.display= "block";
 })
+
+// // revisar boton de volver
+// const backBtn = document.getElementById("backBtn");
+// backBtn.addEventListener("click", function(){
+//     const charactersSection = document.getElementById("charactersSection");
+//     charactersSection.style.display= "none";
+//     const welcomeSection = document.getElementById("welcomePage");
+//     welcomeSection.style.display= "block";
+// })
