cecf7621d6a8b1439220765f2f909e507448ee59
RaizaLR <raizalongart@gmail.com>
2021-01-04 20:36:03 -0300

    formateamos texto

--- src/data.js
+++ src/data.js
@@ -1,48 +1,56 @@
 const functions = {
-  
- orderAZ(info) {
-  let orderedInfo = info.sort(function (a, b) {
-         if (a.name > b.name) {
-           return 1;}
-         if (a.name < b.name) {
-           return -1;}
-         // a debe ser igual a b
-         return 0});  
-     return orderedInfo;
-},
+
+  orderAZ(info) {
+    let orderedInfo = info.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      // a debe ser igual a b
+      return 0
+    });
+    return orderedInfo;
+  },
 
   orderZA(info) {
-  let  orderedInfo = info.sort(function (a, b) {
-           if (a.name < b.name) {
-             return 1}
-           if (a.name > b.name) {
-             return -1}
-           // a debe ser igual a b
-           return 0});   
-        return orderedInfo;
-},
+    let orderedInfo = info.sort(function (a, b) {
+      if (a.name < b.name) {
+        return 1
+      }
+      if (a.name > b.name) {
+        return -1
+      }
+      // a debe ser igual a b
+      return 0
+    });
+    return orderedInfo;
+  },
 
   orderDefault(info) {
-  let orderedInfo = info.sort(function (a, b) {
-           if (a.id > b.id) {
-             return 1}
-           if (a.id < b.id) {
-             return -1}
-           // a debe ser igual a b
-           return 0;
-        });   
-        return orderedInfo;
-},
+    let orderedInfo = info.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1
+      }
+      if (a.id < b.id) {
+        return -1
+      }
+      // a debe ser igual a b
+      return 0;
+    });
+    return orderedInfo;
+  },
 
-speciesFilter(info, species){
-  let speciesArray = info.filter(e => e.species===species);
-  return speciesArray;
-},
+  speciesFilter(info, species) {
+    let speciesArray = info.filter(e => e.species === species);
+    return speciesArray;
+  },
 
-genderFilter(info, gender){
-  let genderArray = info.filter(e => e.gender === gender);
-  return genderArray;
-},
+  genderFilter(info, gender) {
+    let genderArray = info.filter(e => e.gender === gender);
+    return genderArray;
+  },
 }
 
 export default functions;
